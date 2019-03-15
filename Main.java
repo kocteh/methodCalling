4afbfab766b4634554c148c76fea5cd5912501f6
kocteh <kocteh@gmail.com>
2019-03-15 10:30:18 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,29 +7,48 @@ public class Main {
     public static void main(String args[]) {
         Scanner userInput = new Scanner(System.in);
 
-        double priceInLeva = userInput.nextFloat();
-        double customerPaid = userInput.nextFloat();
+        double priceInLeva = Math.round(userInput.nextFloat()*100.0)/100.0;
+        double customerPaid = Math.round(userInput.nextFloat());
         customerChange = Math.round((customerPaid - priceInLeva)*100.0)/100.0;
+
+
+        changeFunction(1);
+        changeFunction(0.5);
+        changeFunction(0.2);
+        changeFunction(0.1);
+        changeFunction(0.05);
+        changeFunction(0.02);
+        changeFunction(0.01);
     }
 
-    public double changeFunction(double coins) {
+    public static double changeFunction(double coins) {
 
         int counter = 0;
 
         while (coins <= (customerChange)) {
             counter++;
-            customerChange = Math.round((customerChange - 1)*100.0)/100.0;
+            customerChange = Math.round((customerChange - coins)*100.0)/100.0;
         }
 
         if (counter > 0) {
-            System.out.println(counter + " x 1 lev");
+            if (coins == 1) {
+                System.out.println(counter + " x 1 lev");
+            } else if (coins == 0.5) {
+                System.out.println(counter + " x 50 stotinki");
+            } else if (coins == 0.2) {
+                System.out.println(counter + " x 20 stotinki");
+            } else if (coins == 0.1) {
+                System.out.println(counter + " x 10 stotinki");
+            } else if (coins == 0.05) {
+                System.out.println(counter + " x 5 stotinki");
+            } else if (coins == 0.02) {
+                System.out.println(counter + " x 2 stotinki");
+            } else if (coins == 0.01) {
+                System.out.println(counter + " x 1 stotinka");
+            }
+
         }
 
         return customerChange;
-
-    }
-
-    public void changeFunctionCalling() {
-        changeFunction(1);
     }
 }
